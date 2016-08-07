d655f22750a3be87310bc18e05f4e85617aff1f9
Milos Marinkovic <milosmns@gmail.com>
2016-08-07 17:21:48 +0200

    Adding a flush (not working without it)

--- src/main/java/co/timecrypt/api/EmailServlet.java
+++ src/main/java/co/timecrypt/api/EmailServlet.java
@@ -65,6 +65,7 @@ public class EmailServlet extends HttpServlet {
             String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
             out.println(docType + "<html>\n" + "<head><title>" + title + "</title></head>\n" + "<body bgcolor=\"#f0f0f0\">\n"
                     + "<h1 align=\"center\">" + title + "</h1>\n" + "<p align=\"center\">" + res + "</p>\n" + "</body></html>");
+            out.flush();
         } catch (MessagingException mex) {
             mex.printStackTrace();
         }
