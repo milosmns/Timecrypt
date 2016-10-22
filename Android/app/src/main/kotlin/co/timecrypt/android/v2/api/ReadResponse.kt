5d8e26080fe22630773e785b5e2134085bd0bb9d
Milos Marinkovic <milosmns@gmail.com>
2016-10-22 13:36:24 +0200

    Adding a dummy wrapper for READ response

---
+++ Android/app/src/main/kotlin/co/timecrypt/android/v2/api/ReadResponse.kt
@@ -0,0 +1,14 @@
+package co.timecrypt.android.v2.api
+
+/**
+ * Interpretation of the `/create` endpoint response from the Timecrypt server. A sample response is:
+ * ```
+ * {
+ *     "id": "f78a89",
+ *     "status_code": 0
+ * }
+ * ```
+ */
+class ReadResponse(
+        // MM fill in
+)
